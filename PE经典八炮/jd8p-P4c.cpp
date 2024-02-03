047e3c3634a897c4fda9cc34d68063dbef0ec3fd
whatss7 <363773602@qq.com>
2024-02-03 11:33:56 +0800

    将经典八炮P4重命名为P4c

--- PE经典八炮/jd8p-P4c.cpp
+++ PE经典八炮/jd8p-P4c.cpp
@@ -1,8 +1,7 @@
 #include "../whatss7-avz2-lib/walib.h"
 
 void AScript(){
-    WAInit({APUFF_SHROOM, ASUN_SHROOM, ASCAREDY_SHROOM, AFLOWER_POT, APUMPKIN}, "Cycle");
-    WACheck();
+    WAInit({APUFF_SHROOM, ASUN_SHROOM, ASCAREDY_SHROOM, AFLOWER_POT, APUMPKIN});
     WAAutoManageCob();
     aPlantFixer.Start(APUMPKIN);
     aPlantFixer.SetHp(4000 / 3 * 2);
