a2c0fd368ee77b081a14a7822524eada44366183
whatss7 <363773602@qq.com>
2024-02-26 13:00:47 +0800

    修复RE神之七炮部分问题

--- RE神之七炮/sideless7p.cpp
+++ RE神之七炮/sideless7p.cpp
@@ -1,10 +1,11 @@
 #include "../whatss7-avz2-lib/walib.h"
 
 void AScript() {
-    WAInit({AICE_SHROOM, AM_ICE_SHROOM, ADOOM_SHROOM, ACOFFEE_BEAN, AFLOWER_POT, ACHERRY_BOMB, AKERNEL_PULT, ACOB_CANNON});
+    WAInit({AICE_SHROOM, AM_ICE_SHROOM, ACOFFEE_BEAN, AFLOWER_POT, ACHERRY_BOMB, AJALAPENO, AKERNEL_PULT, ACOB_CANNON});
     WAAutoManageCob();
     WAFixCobs();
     aIceFiller.Start({{2, 3}, {4, 3}});
+    // ASetZ
     
     // I-PP|IP-PP|PP (15.5, 12.5, 7)
     // 15.5: 完美预判冰下，小丑不会炸到7列的最长波长为1551.
@@ -15,7 +16,7 @@ void AScript() {
     const int a_len = 700;
 
     for (int w: {1, 4, 7, 11, 14, 17}) {
-        TempI(w, 1, 1, 1, w == 11 ? 601 : i1_len);
+        I(w, 1, w == 11 ? 601 : i1_len);
         RoofP(w, 400, 3, 3, 8.5625);
         RoofP(w, i2_len - 200, 1, 2, 9);
         RoofP(w, i2_len - 200, 6, 4, 9);
@@ -25,7 +26,7 @@ void AScript() {
         RoofP(w, a_len - 200, 6, 4, 9);
     }
     for (int w: {3, 6, 9, 13, 16, 19}) {
-        TempI(w, 1, 1, 1, a_len);
+        I(w, 1, a_len);
         RoofP(w, i1_len - 200, 1, 2, 9);
         RoofP(w, i1_len - 200, 6, 4, 9);
         if (w == 9 || w == 19) {
@@ -35,6 +36,7 @@ void AScript() {
             RoofP(w, i1_len + i2_len - 200, 6, 4, 7.5);
             RoofP(w, i1_len + i2_len + a_len - 200, 1, 2, 7);
             RoofP(w, i1_len + i2_len + a_len - 200, 6, 4, 7);
+            // 防止原速小鬼偷家，种两个花盆
             TempC(w, 0, AFLOWER_POT, 1, 3, 4500);
             TempC(w, 800, AFLOWER_POT, 5, 3, 3700);
         }
@@ -44,14 +46,15 @@ void AScript() {
         RoofP(w, 401, 6, 4, 9);
     }
     for (int w: {20}) {
-        TempI(w, 400, 1, 1);
+        I(w, 400);
         RoofP(w, 750, 3, 3, 7);
-        RoofP(w, i2_len - 200, 1, 2, 9);
-        RoofP(w, i2_len - 200, 6, 4, 9);
+        // 400冰下小丑炸7列时机1070
+        RoofP(w, 1060, 1, 2, 9);
+        RoofP(w, 1060, 6, 4, 9);
         A(w, 2050, 3, 8);
-        RoofP(w, i2_len + a_len - 200, 1, 2, 9);
-        RoofP(w, i2_len + a_len - 200, 6, 4, 9);
-        RoofP(w, i2_len + a_len + a_len - 200, 1, 2, 9);
-        RoofP(w, i2_len + a_len + a_len - 200, 6, 4, 9);
+        RoofP(w, i2_len + a_len - 200, 1, 2, 8.5);
+        RoofP(w, i2_len + a_len - 200, 6, 4, 8.5);
+        RoofP(w, i2_len + a_len + a_len - 200, 1, 2, 8);
+        RoofP(w, i2_len + a_len + a_len - 200, 6, 4, 8);
     }
 }
