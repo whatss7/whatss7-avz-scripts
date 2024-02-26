5b44b97be4ca83a4dd2930008700e65f24793e53
whatss7 <363773602@qq.com>
2024-02-26 13:19:35 +0800

    完成PE空炸十八炮

---
+++ PE空炸十八炮/ab18p.cpp
@@ -0,0 +1,23 @@
+#include "../whatss7-avz2-lib/walib.h"
+
+void AScript() {
+    WAInit({AICE_SHROOM, ACOFFEE_BEAN, ACHERRY_BOMB});
+    for (int w: WaveList(1, 20)) {
+        PP(w);
+        P(w, PCP + 110, 1, 8);
+        if (w == 10) SmartA(w, PCP);
+        if (w == 20) P(w, 250, 4, 7.5875);
+        if (w == 9 || w == 19 || w == 20) {
+            ForEnd(w, PCP + 601 - CFT, [w](){
+                PP(w, PCP + 601);
+                P(w, PCP + 601 + 110, 1, 8);
+            });
+            ForEnd(w, PCP + 601 * 2 - CFT, [w](){
+                PP(w, PCP + 601 * 2);
+            });
+            ForEnd(w, PCP + 601 * 3 - CFT, [w](){
+                PP(w, PCP + 601 * 3);
+            });
+        }
+    }
+}
