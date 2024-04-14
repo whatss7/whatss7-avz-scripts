023a29542ef78d5b73b1dfe9815de5ceb926b49f
whatss7 <363773602@qq.com>
2024-04-14 21:28:45 +0800

    翻修DE双冰六炮

--- DE双冰六炮/2i6p.cpp
+++ DE双冰六炮/2i6p.cpp
@@ -1,5 +1,7 @@
 #include "../walib.h"
 
+ATickRunner slowGigaRunner;
+
 void AScript(){
     Init({
         AICE_SHROOM, AM_ICE_SHROOM, ACOFFEE_BEAN, ACHERRY_BOMB, ADOOM_SHROOM,
@@ -18,6 +20,38 @@ void AScript(){
         if (w == 9 || w == 19) {
             PP(w, i_len + i_len - 200, 8.5);
             PP(w, i_len + i_len + 601 - 200, 8.5);
+            AConnect(ATime(w, 2805), [](){
+                slowGigaRunner.Start([](){
+                    for (APlantType plant: { APUFF_SHROOM, ASUN_SHROOM, ASCAREDY_SHROOM, AFLOWER_POT }) {
+                        if (!AGetSeedPtr(plant) || !AGetSeedPtr(plant)->IsUsable()) continue;
+                        AZombie *fastestGiga = nullptr;
+                        for (auto &&zombie: aAliveZombieFilter) {
+                            // 不是红眼不垫
+                            if (zombie.Type() != AHY_32) continue;
+                            // 巨人若是在投掷小鬼或举锤，垫了也没用
+                            if (zombie.State() == 69 || zombie.State() == 70) continue;
+                            // 求得以上条件下最快的僵尸
+                            if (!fastestGiga || zombie.Abscissa() < fastestGiga->Abscissa()) {
+                                fastestGiga = &zombie;
+                            }
+                        }
+                        // 若巨人锤得到七列植物，垫
+                        if (fastestGiga && fastestGiga->Abscissa() < 595) {
+                            int row = fastestGiga->Row() + 1;
+                            ACard(plant, row, 7);
+                        }
+                        return;
+                    }
+                });
+            });
+            AConnect(ATime(w + 1, PCP), [](){
+                slowGigaRunner.Stop();
+                ARemovePlant(1, 7, { APUFF_SHROOM, ASUN_SHROOM, ASCAREDY_SHROOM, AFLOWER_POT });
+                ARemovePlant(2, 7, { APUFF_SHROOM, ASUN_SHROOM, ASCAREDY_SHROOM, AFLOWER_POT });
+                ARemovePlant(3, 7, { APUFF_SHROOM, ASUN_SHROOM, ASCAREDY_SHROOM, AFLOWER_POT });
+                ARemovePlant(4, 7, { APUFF_SHROOM, ASUN_SHROOM, ASCAREDY_SHROOM, AFLOWER_POT });
+                ARemovePlant(5, 7, { APUFF_SHROOM, ASUN_SHROOM, ASCAREDY_SHROOM, AFLOWER_POT });
+            });
         }
     }
 
