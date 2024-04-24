81c8d36506cb96aec9b65fd603874c17dc465bbd
whatss7 <363773602@qq.com>
2024-04-24 15:00:58 +0800

    完善RE椭盘十四炮存冰问题

--- RE椭盘十四炮/ellipse14p.cpp
+++ RE椭盘十四炮/ellipse14p.cpp
@@ -32,15 +32,27 @@ void AScript() {
     StartIceFiller({{2, 3}, {4, 6}, {1, 1}});
     const int i_len = 1800;
     for (int w: {1}) {
-        TempC(w, -599, AFLOWER_POT, 3, 9);
-        TempC(w, -599 + 751, AFLOWER_POT, 4, 9);
-        C(w, -599 + 751 + 751, AFLOWER_POT, 1, 1);
-        RoofP(w, 250, 1, 4, 9.125);
-        N(w, 270, 3, 9);
-        A(w, 270 + 110, 4, 9);
-        RoofP(w, 270 + 215, 7, 2, 5);
+        TempC(w, -599, AFLOWER_POT, 1, 1);
+        C(w, -599 + 751, AFLOWER_POT, 3, 9);
+        // TempC(w, -599 + 751 + 751, AFLOWER_POT, 1, 1);
+        RoofP(w, 280, 1, 4, 9.075);
+        N(w, 450, 3, 9);
+        RoofP(w, 450 + 110, 6, 4, 9);
+        RoofP(w, 450 + 215, 7, 2, 5);
     }
-    for (int w: {2, 10}) {
+    for (int w: {2}) {
+        // PPDDDD, 第二个D解决下波冰车
+        // 上半第一D不能全伤三行，不然炸不到冰车
+        ZomboniA(w, 379, 2, 9);
+        DynamicP(w, 379, 4, 9);
+        RoofP(w, 379 + 119, 2, 2, 8.4);
+        RoofP(w, 379 + 119, 4, 4, 8.45);
+        RoofP(w, 379 + 119 + 137, 1, 2, 8.7);
+        RoofP(w, 379 + 119 + 137, 4, 4, 8.8125);
+        // w10 炸蹦极
+        if (w == 10) RoofP(w, 401, 4, 3, 4.5);
+    }
+    for (int w: {10}) {
         // PPDDDD, 第二个D解决下波冰车
         // 上半第一D不能全伤三行，不然炸不到冰车
         DynamicP(w, 379, 2, 9);
@@ -56,13 +68,14 @@ void AScript() {
         // I-B-PP
         I3(w, 11, 601);
         TempC(w, 950, AFLOWER_POT, 2, 9, 1150);
+        C(w, 950 + 751, AFLOWER_POT, 1, 1);
         RoofP(w, 1140, 7, 4, 8.1);
-        DynamicP(w, 1800 - 200, 2, 9);
-        RoofP(w, 1800 - 200, 6, 6, 9);
+        DynamicP(w, i_len - 200, 2, 9);
+        RoofP(w, i_len - 200, 6, 6, 9);
     }
     for (int w: {4, 12}) {
         // I3
-        I3(w, 11, 1800);
+        I3(w, 11, i_len);
         // Pd/PD，上半解决漏的跳跳
         RoofP(w, 410, 2, 2, 9);
         RoofP(w, 410, 4, 4, 9);
@@ -71,7 +84,7 @@ void AScript() {
         else A(w, 410 + 215, 4, 9);
         RoofP(w, 410 + 320, 1, 2, 5);
         // 垫一下小丑
-        if (w == 3) TempC(w, 950, AFLOWER_POT, 2, 9, 1150);
+        if (w == 4) TempC(w, 950, AFLOWER_POT, 2, 9, 1150);
         // B
         RoofP(w, 1140, 7, 4, 8.1);
         // PP
@@ -113,8 +126,9 @@ void AScript() {
         //     DynamicP(w, 1085 + 200 + 220, 2, 8.95);
         //     RoofP(w, 1085 + 200 + 220, 6, 4, 8.95);
         // } else {
-            // 巨人1282才能对9普通举锤，篮球1091碾压，1039全伤巨人，可以1085生效
-            // PA-N-PP
+        // 巨人1282才能对9普通举锤，篮球1091碾压，1039全伤巨人，可以1085生效
+        // PA-N-PP
+        // FIXME: 拦截存在问题
         C(w, 1085 - ADT - 751, AFLOWER_POT, 2, 8);
         A(w, 1085, 2, 9);
         RoofP(w, 1085, 7, 4, 9);
