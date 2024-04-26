8a422a7725eb0abe3632ce14813efa56fc3238a1
whatss7 <363773602@qq.com>
2024-04-27 02:31:56 +0800

    添加4级与5级阵图，完成ME田八炮C9u

--- ME田八炮/tian8p-c9u.cpp
+++ ME田八炮/tian8p-c9u.cpp
@@ -2,36 +2,36 @@
 
 int crate_pos = 2;
 
-void NextCratePos(int wave, int time) {
-    AConnect(ATime(wave, time), [](){
-        crate_pos = (crate_pos - 1) % 3 + 2;
-    });
-}
-
 void AScript() {
     Init({AICE_SHROOM, AM_ICE_SHROOM, ADOOM_SHROOM, ASQUASH, AFLOWER_POT, AUMBRELLA_LEAF});
 
     const int i_len = 1250, a_len = 601;
     // Ia-PP | PP | Ia-PP | PP | N | PP | PP
 
+    // w1-w9: PP|Ia-PP|PP|IA-PP|PP|N|PP|PP|I-PP(|PP|I-PP|PP)
+    // w10-w19: PP|PP|Ia-PP|PP|IA-PP|PP|N|PP|PP|I-PP(|PP|I-PP|PP)
+
     C(1, -599, AFLOWER_POT, 3, 3);
-    for (int w: { 1, 2, 4, 6, 7, 9, 10, 12, 14, 16, 17, 19 }) {
+    AtEnd([](){ ARemovePlant(3, 3, AFLOWER_POT); });
+    TempC(10, 300 - 751, AUMBRELLA_LEAF, 2, 4, 400);
+    TempC(10, 300, AUMBRELLA_LEAF, 4, 4, 400);
+    TempC(20, 300 - 751, AUMBRELLA_LEAF, 2, 4, 400);
+    TempC(20, 300, AUMBRELLA_LEAF, 4, 4, 400);
+    for (int w: { 1, 3, 5, 7, 8, 10, 11, 13, 15, 17, 18 }) {
         PP(w, a_len - 200);
-        if (w == 9 || w == 19) {
-            PP(w, a_len + i_len - 200);
-            PP(w, a_len + i_len + a_len - 200);
-            N(w, a_len + i_len + a_len * 2 - 200, crate_pos, 9);
-            NextCratePos(w, a_len + i_len + a_len * 2 - 200 + 1);
+    }
+    for (int w: { 2, 4, 9, 12, 14, 19, 20 }) {
+        ManualI(w, 1, 3, 3, a_len);
+        if (w == 2 || w == 12) TempC(w, 650 - SDT, ASQUASH, w == 2 ? 2 : 3, 8, 1250);
+        else if (w == 4 || w == 14) A(w, 650, 3, 8);
+        PP(w, i_len - 200);
+        if (w == 9 || w == 19 || w == 20) {
+            PP(w, i_len + a_len - 200);
+            PP(w, i_len + a_len + i_len - 200);
+            PP(w, i_len + a_len + i_len + a_len - 200);
         }
     }
-    for (int w: { 3, 8, 11, 13, 18 }) {
-        ManualI(w, 1, 3, 3, a_len);
-        if (w == 13) A(w, 650, 3, 8);
-        else TempC(w, 650 - SDT, ASQUASH, crate_pos, 8, 651);
-        PP(w, i_len - 200, 8.5);
-    }
-    for (int w: { 5, 15 }) {
-        ZomboniN(w, a_len - 200, crate_pos, 9);
-        NextCratePos(w, a_len - 200 + 1);
+    for (int w: { 6, 16 }) {
+        ZomboniN(w, a_len - 200, w == 6 ? 2 : 3, 9);
     }
 }
